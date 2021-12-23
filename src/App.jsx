5f3d3be732245a051cde5af7bf77aaf2245890eb
oscar-coderookie <industrialdesignoscar@gmail.com>
2021-12-23 18:24:32 -0500

    ADD: cookies advice

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import {Header} from "./components";
+import CookieConsent from "react-cookie-consent";
 import whatsappLogo from "./assets/img/whatsapp-logo.svg";
 
 function App() {
@@ -21,7 +22,16 @@ function App() {
         <a href="https://api.whatsapp.com/send?phone=+34722355841" target="_blank" rel="noopener noreferrer">
           <img className="whatsapp" src={whatsappLogo} alt="whatsapp" />
         </a>
+        <CookieConsent 
+        debug={true}
+        buttonText="Acepto"
+        style={{ background: "#383838" }}
+        buttonStyle={{ color: "black",  fontSize: "12px" }}
+        >Este sitio web usa cookies. Revisa la política de privacidad para mayor información.
+        </CookieConsent>
       </div>
+
+
     </Router>
   );
 }
