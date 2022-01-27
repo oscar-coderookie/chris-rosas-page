b03dd2c6c2b0b827b56e9a618f9689f0d6bdc4d5
oscar-coderookie <industrialdesignoscar@gmail.com>
2022-01-27 08:44:51 -0500

    ADD: modify homepage and logo

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./HomePage.scss";
 import video from "../../assets/video/intro-chris-rosas.mp4";
+import logo from './../../assets/img/logo-blanco.png';
+
+const VideoHome = ()=> {
+  <video className="homepage__video" loop autoPlay muted>
+        <source src={video} type="video/mp4" />
+      </video>
+}
 
 const HomePage = () => {
   return (
     <div className="homepage">
-      <video className="homepage__video" loop autoPlay muted>
-        <source src={video} type="video/mp4" />
-      </video>
+      <img className="homepage__logo" src={logo} alt="logo" />
     </div>
   );
 };
