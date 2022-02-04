0b03cf980a7297f637c15c6220ad318b6b83f031
oscar-coderookie <industrialdesignoscar@gmail.com>
2022-02-04 16:08:14 -0500

    ADD: gallery of daddy and natti

--- src/pages/DaddyYankee/DaddyYankee.jsx
+++ src/pages/DaddyYankee/DaddyYankee.jsx
@@ -1,6 +1,40 @@
 import React from "react";
-import { SocialBar } from "../../components";
+import { SocialBar, GalleryArtists } from "../../components";
 import "./DaddyYankee.scss";
+
+const images = [
+  {
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F49526027_891848041206688_3830372954828196660_n-824x1030.jpg?alt=media&token=a4469f4d-1435-492b-b5f4-4452b8c5878f"
+  },
+  {
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F20759053_1940812436194343_8193692729021038592_n-823x1030.jpg?alt=media&token=ff4c3392-f2cf-4995-9c71-0d39c93caf3f"
+  },
+  { 
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F40083440_481888528956694_276583818034033985_n-1030x1030.jpg?alt=media&token=db47b62c-8215-4645-95f5-917d58a25683"
+  },
+  { 
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F52847535_154697712212274_3725693517783449125_n-825x1030.jpg?alt=media&token=8762e57e-e0ce-4903-8da3-a92ff70f15a6"
+  },
+  { 
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F49854164_2069419950014672_9151986397733790384_n-944x1030.jpg?alt=media&token=cc431c13-ad2e-402d-b7c8-1a087f20589e"
+  },
+  { 
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F74801823_120800006058907_6310755547999264405_n-705x705.jpg?alt=media&token=618a19a8-bf3c-4502-9502-12474eee0224"
+  },
+  { 
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F74536469_442161353119127_5117533859139601328_n-825x1030.jpg?alt=media&token=41c44ef2-e705-4c4a-a02e-35f0e203b850"
+  },
+  { 
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F74891086_554308905407051_6717204841403463196_n-705x705.jpg?alt=media&token=9ed6ec09-609e-44a6-97cd-c1f2e64f0790"
+  },
+  { 
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F75214711_575111276598901_203129582017561212_n-564x705.jpg?alt=media&token=fa02dfc8-a6c5-4808-9c28-fcef5c345bd6"
+  },
+  { 
+    original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fdaddy-yankee%2F75640841_2674343782659971_8025759995263852029_n-564x705.jpg?alt=media&token=78125be5-a029-48b6-81cf-fb436cc52c2c"
+  },
+
+]
 
 const DaddyYankee = () => {
   return (
@@ -15,6 +49,7 @@ const DaddyYankee = () => {
         spotyLink="https://open.spotify.com/artist/4VMYDCV2IEDYJArk749S6m"
 
       />
+      <GalleryArtists images={images} />
     </div>
   );
 };
--- src/pages/NattiNatasha/NattiNatasha.jsx
+++ src/pages/NattiNatasha/NattiNatasha.jsx
@@ -1,6 +1,20 @@
 import React from "react";
-import { SocialBar } from "../../components";
+import { SocialBar, GalleryArtists } from "../../components";
 import "./NattiNatasha.scss";
+
+const images = [
+
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F77292296_281549499454120_7112610377239675043_n-705x705.jpg?alt=media&token=feeae5c2-7cc6-4cca-a675-a61a6d4430c6" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F71910469_1795357117263245_1029615508408768772_n-824x1030.jpg?alt=media&token=3b8078db-47e7-45fa-9a4e-aab38cac446a" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F72216252_238155023819300_5001751198351316981_n-824x1030.jpg?alt=media&token=943b22ac-2b79-4ccf-b5e6-540034fca876" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F72308169_125288971871246_8823984978453134966_n-564x705.jpg?alt=media&token=5a51ade3-aed3-4197-84fc-1e3f26009fc6" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F72336322_419105838761215_859062624597998637_n-566x705.jpg?alt=media&token=1b081031-0479-4de1-b737-14c7da078d62" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F72701490_3139823906059854_6346396631706354477_n-564x705.jpg?alt=media&token=5463d494-8464-49e5-b6ae-07df038c106d" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F72900359_2309876485808111_109583339599425580_n-564x705.jpg?alt=media&token=2e6f91d2-745e-4808-89d9-87698a932279" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F73387307_104533870959079_1428518892702150119_n-705x705.jpg?alt=media&token=865b9921-22c3-4f4f-a3cc-f1af1116c36c" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F73455965_433942820567977_6040788848034286508_n-564x705.jpg?alt=media&token=7fa198dd-27f7-4310-aa84-88fb194ef9f1" },
+  { original: "https://firebasestorage.googleapis.com/v0/b/chris-rosas-web.appspot.com/o/artistas%2Fnatti-natasha%2F74905871_170079570767899_8170043166864929891_n-705x705.jpg?alt=media&token=ba5e5e00-bfc9-4294-9087-7decedb901ca" },
+]
 
 const NattiNatasha = () => {
   return (
@@ -14,6 +28,7 @@ const NattiNatasha = () => {
         igLink="https://www.instagram.com/nattinatasha/?hl=es"
         spotyLink="https://open.spotify.com/artist/1GDbiv3spRmZ1XdM1jQbT7"
       />
+      <GalleryArtists images={images}/>
     </div>
   );
 };
